97c06c1f29e639f008dbc17b6605de36dead7cc4
Salvatore Coppola <salvatore.coppola@eurotech.com>
2022-10-13 09:59:44 +0200

    Read MAX_LENGTH_INPLACE_LOB from settings.

--- h2/src/main/org/h2/engine/Database.java
+++ h2/src/main/org/h2/engine/Database.java
@@ -227,7 +227,8 @@ public final class Database implements DataHandler, CastDataProvider {
         this.filePasswordHash = ci.getFilePasswordHash();
         this.databaseName = databaseName;
         this.databaseShortName = parseDatabaseShortName();
-        this.maxLengthInplaceLob = Constants.DEFAULT_MAX_LENGTH_INPLACE_LOB;
+        this.maxLengthInplaceLob = 
+                ci.getProperty("MAX_LENGTH_INPLACE_LOB", persistent ? Constants.DEFAULT_MAX_LENGTH_INPLACE_LOB : Integer.MAX_VALUE - 8);
         this.cipher = cipher;
         this.autoServerMode = ci.getProperty("AUTO_SERVER", false);
         this.autoServerPort = ci.getProperty("AUTO_SERVER_PORT", 0);
@@ -1249,8 +1250,10 @@ public final class Database implements DataHandler, CastDataProvider {
      */
     private synchronized void closeOpenFilesAndUnlock() {
         try {
-            lobStorage.close();
-            if (!store.getMvStore().isClosed()) {
+            if (lobStorage != null) {
+                lobStorage.close();
+            }
+            if (store != null && !store.getMvStore().isClosed()) {
                 if (compactMode == CommandInterface.SHUTDOWN_IMMEDIATELY) {
                     store.closeImmediately();
                 } else {
@@ -1260,13 +1263,13 @@ public final class Database implements DataHandler, CastDataProvider {
                             dbSettings.defragAlways ? -1 : dbSettings.maxCompactTime;
                     store.close(allowedCompactionTime);
                 }
-            }
-            if (persistent) {
-                // Don't delete temp files if everything is already closed
-                // (maybe in checkPowerOff), the database could be open now
-                // (even from within another process).
-                if (lock != null || fileLockMethod == FileLockMethod.NO || fileLockMethod == FileLockMethod.FS) {
-                    deleteOldTempFiles();
+                if (persistent) {
+                    // Don't delete temp files if everything is already closed
+                    // (maybe in checkPowerOff), the database could be open now
+                    // (even from within another process).
+                    if (lock != null || fileLockMethod == FileLockMethod.NO || fileLockMethod == FileLockMethod.FS) {
+                        deleteOldTempFiles();
+                    }
                 }
             }
         } finally {
@@ -1775,12 +1778,10 @@ public final class Database implements DataHandler, CastDataProvider {
      * that thread, throw it now.
      */
     void throwLastBackgroundException() {
-        if (!store.getMvStore().isBackgroundThread()) {
-            DbException b = backgroundException.getAndSet(null);
-            if (b != null) {
-                // wrap the exception, so we see it was thrown here
-                throw DbException.get(b.getErrorCode(), b, b.getMessage());
-            }
+        DbException b = backgroundException.getAndSet(null);
+        if (b != null) {
+            // wrap the exception, so we see it was thrown here
+            throw DbException.get(b.getErrorCode(), b, b.getMessage());
         }
     }
 
